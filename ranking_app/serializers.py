9c7a2002b0780a752dff6323e27a4ee76385de48
domii <eiji.okumura2005@gmail.com>
2026-07-19 15:11:38 -0400

    Delete ranking_app directory

--- ranking_app/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import CatRanking
-
-class CatRankingSerializer(serializers.ModelSerializer):
-    nombre_del_gato = serializers.StringRelatedField(source='gato')
-
-    class Meta:
-        model = CatRanking
-        fields = ['id', 'gato', 'nombre_del_gato', 'puntos']
