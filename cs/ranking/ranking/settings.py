674cff1a09cc08e5d3556bfca865c8070fb47723
domii <eiji.okumura2005@gmail.com>
2026-07-19 11:43:02 -0400

    Secret Key settings.py

--- cs/ranking/ranking/settings.py
+++ cs/ranking/ranking/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x4-h(mafx^8-!r8-*++l%&91+e8*hn9pkot#bc1w7g_u#6cu8w"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-x4-h(mafx^8-!r8-*++l%&91+e8*hn9pkot#bc1w7g_u#6cu8w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
