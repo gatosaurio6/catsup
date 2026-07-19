5cc5a24de6f94692f0717df5236e2fc6c7fabdf4
NiqoPulento <elnicolassoldini@gmail.com>
2026-07-18 20:17:31 -0400

    aoa si

--- cs/login/backend/settings.py
+++ cs/login/backend/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'login',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- cs/login/login/urls.py
+++ cs/login/login/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView
 
 urlpatterns = [
-    # Esta es la ruta exacta que tu compañera está consumiendo desde el frontend
-    path('', TokenObtainPairView.as_view(), name='login'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Ruta registrar nuevo usuario
+    path('register/', RegisterView.as_view(), name='register'),
+    
+    # Ruta inicio sesión
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
+    # Ruta refresh
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
